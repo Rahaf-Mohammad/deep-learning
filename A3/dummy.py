637585c731170ea6335816bb0e4f7c7cd27fa1a6
Rahaf-Mohammad <4310386@upm.edu.sa>
2026-02-14 03:51:21 -0800

    Update dummy.py

--- A3/dummy.py
+++ A3/dummy.py
@@ -13,10 +13,10 @@ class SimpleFeedForwardNet(tr.nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.linear1 = tr.nn.Linear(784, 256)   # من الصورة (28x28)
+        self.linear1 = tr.nn.Linear(784, 256)   
         self.linear2 = tr.nn.Linear(256, 128)
         self.linear3 = tr.nn.Linear(128, 64)
-        self.linear4 = tr.nn.Linear(64, 10)     # 10 كلاسز (FashionMNIST)
+        self.linear4 = tr.nn.Linear(64, 10)  
 
     def forward(self, x):
 
@@ -32,5 +32,6 @@ model = SimpleFeedForwardNet()
 optimizer = tr.optim.SGD(model.parameters(), lr=0.01)
 
 loss_fn = tr.nn.CrossEntropyLoss()
+
 
 
