507611ce606ba0184008f8abf829a82b5ff79608
Rahaf-Mohammad <4310386@upm.edu.sa>
2026-02-14 03:50:17 -0800

    Update dummy.py

--- A3/dummy.py
+++ A3/dummy.py
@@ -13,18 +13,17 @@ class SimpleFeedForwardNet(tr.nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.linear1 = tr.nn.Linear(784, 512, bias=True)
-        self.linear2 = tr.nn.Linear(512, 256, bias=True)
-        self.linear3 = tr.nn.Linear(256, 128, bias=True)
-        self.linear4 = tr.nn.Linear(128, 64, bias=True)
-        self.linear5 = tr.nn.Linear(64, 10, bias=True)
+        self.linear1 = tr.nn.Linear(784, 256)   # من الصورة (28x28)
+        self.linear2 = tr.nn.Linear(256, 128)
+        self.linear3 = tr.nn.Linear(128, 64)
+        self.linear4 = tr.nn.Linear(64, 10)     # 10 كلاسز (FashionMNIST)
 
     def forward(self, x):
+
         x = self.linear1(x)
         x = self.linear2(x)
         x = self.linear3(x)
         x = self.linear4(x)
-        x = self.linear5(x)
         return x
 
 
@@ -33,3 +32,5 @@ model = SimpleFeedForwardNet()
 optimizer = tr.optim.SGD(model.parameters(), lr=0.01)
 
 loss_fn = tr.nn.CrossEntropyLoss()
+
+
